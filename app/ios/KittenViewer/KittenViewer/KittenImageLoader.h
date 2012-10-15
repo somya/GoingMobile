8be7d0551e602cb579cedfe8b6d95b4c25529b3d
Somya Jain <somyaj@gmail.com>
2012-10-15 12:44:45 -0400

    added Loader helper

---
+++ app/ios/KittenViewer/KittenViewer/KittenImageLoader.h
@@ -0,0 +1,16 @@
+//
+//  Created by somya on 10/12/12.
+//
+//
+
+
+#import <Foundation/Foundation.h>
+
+@interface KittenImageLoader : NSObject
+{
+}
+
++ (UIImage *)loadImageFromUrl:(NSString *)url;
+
+
+@end
