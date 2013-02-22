19d0ae7c275b07d6c79f59b00f79c40aac7b4e11
Somya Jain <somyaj@gmail.com>
2013-02-22 14:02:53 -0500

    updated for .net

--- app/mono/KittenViewerTouch/KittenViewCell.cs
+++ app/mono/KittenViewerTouch/KittenViewCell.cs
@@ -1,10 +1,21 @@
 using System;
+using MonoTouch.UIKit;
 
 namespace KittenViewerTouch
 {
-	public class KittenViewCell
+	public class KittenViewCell :  UITableViewCell
 	{
+		public UIImageView LeftIamge { get; set; }
+		public UIImageView RightIamge { get; set; }
+
+		public string LeftUrl { get; set; }
+		public string RightUrl { get; set; }
+
 		public KittenViewCell()
+		{
+		}
+
+		public KittenViewCell( UITableViewCellStyle @default, string cellIdentifier ) : base( @default, cellIdentifier)
 		{
 		}
 	}
