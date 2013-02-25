932575b6de5fdece0dbe399d282ab3bd2be80bc3
Somya Jain <somyaj@gmail.com>
2013-02-24 23:16:48 -0500

    initial merge

---
+++ app/mono/KittenViewerTouch/KittenTableSource.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Drawing;
+
+using MonoTouch.Foundation;
+using MonoTouch.UIKit;
+
+namespace KittenViewerTouch
+{
+    
+    public class KittenTableSource : UITableViewSource
+    {
+
+		public const string cellIdentifier = "KittenCell";
+
+        public override int NumberOfSections(UITableView tableView)
+        {
+			return 1;
+        }
+
+		public override int RowsInSection(UITableView tableview, int section)
+		{
+			return 100;
+		}
+
+		public override float GetHeightForRow(UITableView tableView, NSIndexPath indexPath)
+		{
+			return  150 + ((new Random()).Next() % 150);
+		}
+
+		public override UITableViewCell GetCell(UITableView tableView, NSIndexPath indexPath)
+		{
+			// request a recycled cell to save memory
+			KittenViewCell cell = (KittenViewCell) tableView.DequeueReusableCell (cellIdentifier);
+			// if there are no cells to reuse, create a new one
+			if ( cell == null )
+			{
+				cell = new KittenViewCell( UITableViewCellStyle.Default, cellIdentifier );
+			}
+			return cell;
+		}
+
+		public override void WillDisplay(UITableView tableView, UITableViewCell cell, NSIndexPath indexPath)
+		{
+			if ( cell is KittenViewCell )
+			{
+//				Console.WriteLine( "indexPath={0}", indexPath );
+
+				var kittenCell = (cell as KittenViewCell);
+				kittenCell.Row = indexPath.Row;
+				kittenCell.LoadImages();
+			}
+		}
+    }
+}
---
+++ app/mono/KittenViewerTouch/KittenViewCell.cs
@@ -0,0 +1,96 @@
+using System;
+using MonoTouch.UIKit;
+using MonoTouch.CoreGraphics;
+using System.Drawing;
+using System.Net;
+using MonoTouch.Foundation;
+using System.Threading.Tasks;
+
+namespace KittenViewerTouch
+{
+	public class KittenViewCell :  UITableViewCell
+	{
+		public UIImageViewFromUrl LeftImage { get; set; }
+		public UIImageViewFromUrl RightImage { get; set; }
+
+		public string LeftUrl { get; set; }
+		public string RightUrl { get; set; }
+
+		public int Row { get; set;}
+
+		 UIImage emptyImage = new UIImage("empty.png");
+
+		public KittenViewCell()
+		{
+		}
+
+		public KittenViewCell( UITableViewCellStyle @default, string cellIdentifier ) : base( @default, cellIdentifier)
+		{
+			LeftImage = new UIImageViewFromUrl( emptyImage);
+			RightImage = new UIImageViewFromUrl( emptyImage);
+
+			this.ContentView.Add(LeftImage);
+			this.ContentView.Add(RightImage);
+
+
+		}
+
+		public override void PrepareForReuse()
+		{
+			base.PrepareForReuse();
+			LeftImage.Image = emptyImage;
+			RightImage.Image = emptyImage;
+		}
+
+		public void LoadImages()
+		{
+			var maxX  = Bounds.Width;
+			var randomWidh = 40 + ((new Random()).Next() % (int)(maxX - 80));
+			RectangleF left, right;
+
+			Bounds.Divide(randomWidh, CGRectEdge.MinXEdge, out left, out right);
+
+			LeftImage.Frame = left.Inset(1,1);
+			RightImage.Frame = right.Inset(1,1);
+
+			LoadImage(LeftImage);
+			LoadImage(RightImage);
+		}
+
+		private void LoadImage(UIImageViewFromUrl imageView)
+		{
+
+			imageView.Url =  string.Format( "http://placekitten.com/{0}/{1}", imageView.Bounds.Width * 2, imageView.Bounds.Height * 2 );
+			var row = this.Row;
+			var task = new Task<UIImage>( (url) => 
+			{
+
+				Console.WriteLine( "loading image for Row ={0}", row );
+				if (imageView.Url == url) 
+				{	
+					WebClient client = new WebClient();
+					var image = new UIImage(NSData.FromArray(client.DownloadData( (string)url )));
+					return image;
+				}
+				return null;
+
+			}, imageView.Url);
+
+			task.ContinueWith( t => 
+			{
+
+				InvokeOnMainThread( () =>  
+				{
+					if (imageView.Url == (string)t.AsyncState) 
+					{	
+						imageView.Image = t.Result;
+					}
+				});
+			});
+
+			TaskManager.SubmitTask(task );
+
+		}
+	}
+}
+
